1ab49bdca3fab2b13c868ca391758af55ed51623
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-06-21 09:09:25 +0200

    update

--- include/libcos/_sizes.h
+++ include/libcos/_sizes.h
@@ -1,12 +1,6 @@
 #ifndef _STDC_MACHINE__TYPES_H_
 #define _STDC_MACHINE__TYPES_H_
 
-#ifndef __CHAR_BIT__
-# define __CHAR_BIT__                   8
-#endif
-
-#ifndef __SIZEOF_SHORT__
-# define __SIZEOF_SHORT__               sizeof(short)
-#endif
+#include "private/port.h"
 
 #endif
--- include/libcos/sleep.h
+++ include/libcos/sleep.h
@@ -7,10 +7,6 @@
 extern "C" {
 #endif
 
-#define SYS_CLK_MHZ    (3000)
-#define SYS_CLK_KHZ    (SYS_CLK_MHZ * 1000)
-#define SYS_CLK_HZ     (SYS_CLK_KHZ * 1000)
-
 void udelay(uint32_t usec);
 void mdelay (unsigned msec);
 
