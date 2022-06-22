5c2d5ecc51e3035721fde2c51162c3376f2e29a6
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-06-22 16:40:23 +0200

    upfat

--- include/libcos/endian.h
+++ include/libcos/endian.h
@@ -15,27 +15,6 @@
 
 #define __FLOAT_WORD_ORDER	__BYTE_ORDER
 
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    #define __LONG_LONG_PAIR(HI, LO) LO, HI
-
-    #define	__QUAD_HIGHWORD	1
-    #define	__QUAD_LOWWORD	0
-
-    #define	__htonl(_x)	__bswap32(_x)
-    #define	__htons(_x)	__bswap16(_x)
-    #define	__ntohl(_x)	__bswap32(_x)
-    #define	__ntohs(_x)	__bswap16(_x)
-#elif __BYTE_ORDER == __BIG_ENDIAN
-    #define __LONG_LONG_PAIR(HI, LO) HI, LO
-    #define	__QUAD_HIGHWORD	0
-    #define	__QUAD_LOWWORD	1 
-
-    #define	__htonl(_x)	((uint32_t)(_x))
-    #define	__htons(_x)	((uint16_t)(_x))
-    #define	__ntohl(_x)	((uint32_t)(_x))
-    #define	__ntohs(_x)	((uint16_t)(_x))  
-#endif
-
 #define __WORDSIZE 32
 
 #ifdef __GNUC__
@@ -48,17 +27,85 @@
     }
     static inline uint32_t __bswap32(uint32_t _x) {
         return ((uint32_t)((_x >> 24) | ((_x >> 8) & 0xff00) |
-            ((_x << 8) & 0xff0000) | ((_x << 24) & 0xff000000)));
+                           ((_x << 8) & 0xff0000) | ((_x << 24) & 0xff000000))
+                );
     }
 
     static inline uint64_t __bswap64(uint64_t _x) {
-        return ((uint64_t)((_x >> 56) | ((_x >> 40) & 0xff00) |
-            ((_x >> 24) & 0xff0000) | ((_x >> 8) & 0xff000000) |
-            ((_x << 8) & ((uint64_t)0xff << 32)) |
-            ((_x << 24) & ((uint64_t)0xff << 40)) |
-            ((_x << 40) & ((uint64_t)0xff << 48)) | ((_x << 56))));
+        return ((uint64_t) ((_x >> 56) | ((_x >> 40) & 0xff00) |
+                            ((_x >> 24) & 0xff0000) | ((_x >> 8) & 0xff000000) |
+                            ((_x << 8) & ((uint64_t)0xff << 32)) |
+                            ((_x << 24) & ((uint64_t)0xff << 40)) |
+                            ((_x << 40) & ((uint64_t)0xff << 48)) | ((_x << 56)))
+                );
     }
 #endif
+
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    #define __LONG_LONG_PAIR(HI, LO) LO, HI
+
+    #define	__QUAD_HIGHWORD	1
+    #define	__QUAD_LOWWORD	0
+
+    #define	htonl(_x)	__bswap32(_x)
+    #define	htons(_x)	__bswap16(_x)
+    #define	ntohl(_x)	__bswap32(_x)
+    #define	ntohs(_x)	__bswap16(_x)
+    #define ntohll(_x) __bswap64(_x)
+    #define htonll(_x) __bswap64(_x)
+
+    #define htobe16(_x) __bswap16(_x)
+    #define be16toh(_x) __bswap16(_x)
+    #define betoh16(_x) __bswap16(_x)
+    #define htobe32(_x) __bswap32(_x)
+    #define be32toh(_x) __bswap32(_x)
+    #define betoh32(_x) __bswap32(_x)
+    #define htobe64(_x) __bswap64(_x)
+    #define be64toh(_x) __bswap64(_x)
+    #define betoh64(_x) __bswap64(_x)
+    #define htole16(_x) (uint16_t)(_x)
+    #define le16toh(_x) (uint16_t)(_x)
+    #define letoh16(_x) (uint16_t)(_x)
+    #define htole32(_x) (uint32_t)(_x)
+    #define le32toh(_x) (uint32_t)(_x)
+    #define letoh32(_x) (uint32_t)(_x)
+    #define htole64(_x) (uint64_t)(_x)
+    #define le64toh(_x) (uint64_t)(_x)
+    #define letoh64(_x) (uint64_t)(_x)
+
+#elif __BYTE_ORDER == __BIG_ENDIAN
+    #define __LONG_LONG_PAIR(HI, LO) HI, LO
+    #define	__QUAD_HIGHWORD	0
+    #define	__QUAD_LOWWORD	1 
+
+    #define	htonl(_x)	((uint32_t)(_x))
+    #define	htons(_x)	((uint16_t)(_x))
+    #define	ntohl(_x)	((uint32_t)(_x))
+    #define	ntohs(_x)	((uint16_t)(_x))  
+    #define ntohll(_x)  ((uint64_t)(_x))
+    #define htonll(_x)  ((uint64_t)(_x))
+
+    #define htobe16(_x) (uint16_t)(_x)
+    #define be16toh(_x) (uint16_t)(_x)
+    #define betoh16(_x) (uint16_t)(_x)
+    #define htobe32(_x) (uint32_t)(_x)
+    #define be32toh(_x) (uint32_t)(_x)
+    #define betoh32(_x) (uint32_t)(_x)
+    #define htobe64(_x) (uint64_t)(_x)
+    #define be64toh(_x) (uint64_t)(_x)
+    #define betoh64(_x) (uint64_t)(_x)
+    #define htole16(_x) __bswap16(_x)
+    #define le16toh(_x) __bswap16(_x)
+    #define letoh16(_x) __bswap16(_x)
+    #define htole32(_x) __bswap32(_x)
+    #define le32toh(_x) __bswap32(_x)
+    #define letoh32(_x) __bswap32(_x)
+    #define htole64(_x) __bswap64(_x)
+    #define le64toh(_x) __bswap64(_x)
+    #define letoh64(_x) __bswap64(_x)
+#endif
+
+
 
 
 #endif
