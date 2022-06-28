3f6afe5d5e79055c0739821fe709882b9a70092f
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-06-28 15:43:40 +0200

    add version

---
+++ include/libcos/klibcos.h
@@ -0,0 +1,14 @@
+#ifndef KLIBCOS_VERSIO_H
+#define KLIBCOS_VERSIO_H
+
+#define KLIBCOS_VERSION_MAJOR       1
+#define KLIBCOS_VERSION_MINOR       0
+#define KLIBCOS_VERSION_PATCH       2
+#define KLIBCOS_VERSION_DEBUG       341
+
+#define KLIBCOS_VERSION             0x010002341
+#define KLIBCOS_VERSION_STRING      "1.0.2-non"
+
+#define KLIBCOS_PLATFORM            "non"
+
+#endif
