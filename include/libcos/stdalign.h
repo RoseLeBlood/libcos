400255f9ec9a9c57d27dd49a9ebe004f318ae509
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-06-28 15:37:05 +0200

    add stdalign

---
+++ include/libcos/stdalign.h
@@ -0,0 +1,16 @@
+#ifndef KLIBC_STDALIGN_H
+#define KLIBC_STDALIGN_H
+
+#include "alignment.h"
+
+#ifndef __cplusplus
+
+#define alignas _Alignas
+#define alignof _Alignof
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif
+
+#endif	/* stdalign.h */
