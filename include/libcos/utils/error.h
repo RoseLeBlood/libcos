674fc2e11d592ec7a6fe11f48e905672cdee2748
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-06-29 12:58:19 +0200

    add error header

---
+++ include/libcos/utils/error.h
@@ -0,0 +1,15 @@
+#ifndef KLIBCOS_ERROR_H
+#define KLIBCOS_ERROR_H
+
+#define     KLIBCOS_NOERROR         0
+#define     KLIBCOS_ERRROR          1
+
+
+#define     KLIBCOS_LOCK_OK         KLIBCOS_NOERROR
+#define     KLIBCOS_LOCK_ERROR      KLIBCOS_ERRROR
+
+
+#define     KLIBCOS_STATE_LOCK      0
+#define     KLIBCOS_STATE_UNLOCK    1
+
+#endif
