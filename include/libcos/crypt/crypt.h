63bc9ae0f6bff06ed5886bfa00084c6498d75d1d
Amber-Sophia Schröck <ambersophia.amshop@outlook.de>
2022-07-27 13:50:08 +0200

    update

---
+++ include/libcos/crypt/crypt.h
@@ -0,0 +1,45 @@
+/*
+* MIT License
+*
+* Copyright (c) 2022 Amber-Sophia Schröck
+
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#ifndef __KLIBCOS_CRYPT_H__
+#define __KLIBCOS_CRYPT_H__
+
+#include "../stddef.h"
+#include "../types.h"
+#include "../assert.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+int8_t* crypt_md5(const int8_t* key, const int8_t* setting, int8_t* output);
+int8_t* crypt_blowfish(const int8_t* key, const int8_t* setting, int8_t* output);
+int8_t* crypt_sha256(const int8_t* key, const int8_t* setting, int8_t* output);
+int8_t* crypt_sha512(const int8_t* key, const int8_t* setting, int8_t* output);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif
